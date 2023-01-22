ef65a11220c41ad0beed848ecefef0bb670d5f01
garciamarcoss <garciams1995@gmail.com>
2023-01-21 21:32:07 -0300

    creation of main page and post component

---
+++ src/pages/main/main.tsx
@@ -0,0 +1,33 @@
+import { getDocs, collection } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
+import { db } from '../../config/firebase';
+import { Post } from './post';
+
+export interface Post {
+    id: string;
+    userID: string;
+    title: string;
+    description: string;
+    username: string;
+}
+
+export const Main = () => {
+    const [postList, setPostList] = useState<Post[] | null>(null)
+    const postsRef = collection(db, 'posts')
+    const getPost = async () => {
+        const data = await getDocs(postsRef);
+        setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[])
+    }
+
+    useEffect(() => {
+        getPost()
+    }, [])
+
+    return (
+        <div>
+            {postList?.map((post) => (
+                <Post post={post} />
+            ))}
+        </div>
+    )
+}
